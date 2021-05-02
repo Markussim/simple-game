194707869cd2deb23023c6d67d18d441d75bd126
Markussim <markus.simonsen@gmail.com>
2021-05-02 15:54:08 +0200

    Fixed hold issue

--- main.js
+++ main.js
@@ -10,44 +10,51 @@ let times = [];
 
 let topScore = 0;
 
+let pressed = false;
+
 randomColor();
 
 document.addEventListener("keydown", (e) => {
-  if (e.key == arrow) {
-    colorBox.style.backgroundColor = "green";
-    score++;
-  } else {
-    colorBox.style.backgroundColor = "red";
-    score = 0;
-  }
-
-  let sum = times.reduce(function (a, b) {
-    return a + b;
-  }, 0);
-
-  if (sum > 0) {
-    avg = (1 / (sum / times.length)) * 100000;
-
-    if (avg > topScore || topScore == 0) {
-      topScore = avg;
+  if (pressed == false) {
+    if (e.key == arrow) {
+      colorBox.style.backgroundColor = "green";
+      score++;
+    } else {
+      colorBox.style.backgroundColor = "red";
+      score = 0;
     }
 
-    console.log(topScore);
-    document.getElementById("score").innerText =
-      avg.toFixed(1) + " Highscore: " + parseInt(topScore).toFixed(1);
-  } else {
-    document.getElementById("score").innerText =
-      "Highscore: " + parseInt(topScore).toFixed(1);
-  }
+    let sum = times.reduce(function (a, b) {
+      return a + b;
+    }, 0);
 
-  if (score > 1) {
-    if (times.length > 9) times.shift();
-    times.push(performance.now() - t0);
-  } else {
-    times = [];
-  }
+    if (sum > 0) {
+      avg = (1 / (sum / times.length)) * 100000;
 
-  t0 = performance.now();
+      if (avg > topScore || topScore == 0) {
+        topScore = avg;
+      }
+
+      console.log(topScore);
+      document.getElementById("score").innerText =
+        avg.toFixed(1) + " Highscore: " + parseInt(topScore).toFixed(1);
+    } else {
+      document.getElementById("score").innerText =
+        "Highscore: " + parseInt(topScore).toFixed(1);
+    }
+
+    if (score > 1) {
+      if (times.length > 9) times.shift();
+      times.push(performance.now() - t0);
+    } else {
+      times = [];
+    }
+
+    t0 = performance.now();
+    pressed = true;
+  } else {
+    alert("No not hold");
+  }
 });
 
 document.addEventListener("keyup", (e) => {
@@ -62,4 +69,5 @@ function randomColor() {
     colorBox.style.backgroundColor = "yellow";
     arrow = "a";
   }
+  pressed = false;
 }
